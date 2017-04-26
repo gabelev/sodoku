e31db649dce5ce923fad61a7c2912a7ff7711cd1
Gabe Levine <gabe@g4briel.com>
2017-04-26 19:05:38 -0400

    initial commit

---
+++ sodoku.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BOARD 9
+
+int** read_input(char *file_name);
+int** create_array(int size);
+
+int main(int argc, char* argv[]) 
+{
+    
+    return 0;
+}
+
+int** read_input(char *file_name) 
+{
+    FILE *input_file = fopen(file_name, "r");
+    return 0;
+}
